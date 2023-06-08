152373b330761f1b5fa3de213506cebfab4a2e1a
Samantha Cox <samantha.wright822@gmail.com>
2023-06-08 12:04:20 -0400

    renaming test

--- tests/test_fxns.py
+++ tests/test_fxns.py
@@ -2,6 +2,6 @@ from mdagent.general_tools import dummy_function
 
 
 # dummy test, remove when we have real tests here
-def dummy_test():
+def test_dummy():
     dummy_output = dummy_function()
     assert dummy_output == 46
