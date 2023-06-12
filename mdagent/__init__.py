bd8408d5e879b9d2709b7f1b95b60f81edc9bf58
Samantha Cox <samantha.wright822@gmail.com>
2023-06-12 17:04:28 -0400

    init files

---
+++ mdagent/__init__.py
@@ -0,0 +1,2 @@
+from mdagent.agent.tool_list import MDTools
+from mdagent.general_tools import dummy_function, scholar2result_llm, web_search
---
+++ mdagent/agent/__init__.py
@@ -0,0 +1,2 @@
+from mdagent.agent.agent import MDAgent
+from mdagent.agent.tool_list import MDTools
