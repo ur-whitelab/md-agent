dcee5c492e04c5f0d3e41a23a05e5ec33310c87f
Samantha Cox <samantha.wright822@gmail.com>
2023-06-12 17:20:06 -0400

    added setup back in

---
+++ setup.py
@@ -0,0 +1,32 @@
+from setuptools import find_packages, setup
+
+from mdagent.version import __version__
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name="md-agent",
+    version=__version__,
+    description="Collection of MD tools for use with language models",
+    author="Andrew White",
+    author_email="andrew.white@rochester.edu",
+    url="https://github.com/ur-whitelab/md-agent",
+    license="MIT",
+    packages=find_packages(),
+    install_requires=[
+        "pypdf",
+        "langchain",
+        "paper-qa",
+        "pqapi",
+        "rmrkl",
+    ],
+    test_suite="tests",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
