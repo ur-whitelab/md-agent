644d246088f6b024827241a8bacb4e86c79a19d6
Sam Cox <swrig30@ur.rochester.edu>
2023-12-21 08:56:14 -0800

    combined the two visualization tools into one (#57)

--- mdagent/tools/base_tools/analysis_tools/__init__.py
+++ mdagent/tools/base_tools/analysis_tools/__init__.py
@@ -1,19 +1,13 @@
 from .plot_tools import SimulationOutputFigures
 from .ppi_tools import PPIDistance
 from .rmsd_tools import RMSDCalculator
-from .vis_tools import (
-    CheckDirectoryFiles,
-    PlanBVisualizationTool,
-    VisFunctions,
-    VisualizationToolRender,
-)
+from .vis_tools import CheckDirectoryFiles, VisFunctions, VisualizeProtein
 
 __all__ = [
     "PPIDistance",
     "RMSDCalculator",
     "SimulationOutputFigures",
     "CheckDirectoryFiles",
-    "PlanBVisualizationTool",
+    "VisualizeProtein",
     "VisFunctions",
-    "VisualizationToolRender",
 ]
