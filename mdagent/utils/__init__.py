998e8564bda2ff7af29ed6723e508af354d648b9
Sam Cox <swrig30@ur.rochester.edu>
2024-02-27 10:06:15 -0800

    removed unused function

--- mdagent/utils/__init__.py
+++ mdagent/utils/__init__.py
@@ -1,5 +1,4 @@
-from .general_utils import find_file_path
 from .makellm import _make_llm
 from .path_registry import FileType, PathRegistry
 
-__all__ = ["_make_llm", "PathRegistry", "FileType", "find_file_path"]
+__all__ = ["_make_llm", "PathRegistry", "FileType"]
