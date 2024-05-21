5b0ad57ea4e427d878bc0895dd59a8af7796f302
Jorge <jmedina9@ur.rochester.edu>
2024-05-21 17:02:22 -0400

    attempting to fix test fails

--- tests/test_pdb_tools.py
+++ tests/test_pdb_tools.py
@@ -74,7 +74,6 @@ def test_packmol_download_only_once(packmol):
     # Call the function again with the same molecule
     packmol._get_sm_pdbs(small_molecules)
     os.path.getmtime(f"{packmol.path_registry.ckpt_pdb}/water.pdb")
-    print(f"{packmol.path_registry.ckpt_pdb}/{small_molecules[0]}.pdb")
 
     assert os.path.exists(f"{packmol.path_registry.ckpt_pdb}/{small_molecules[0]}.pdb")
 
@@ -83,7 +82,6 @@ def test_packmol_download_only_once(packmol):
     )
     time.sleep(3)
     packmol._get_sm_pdbs(small_molecules)
-    print(f"{packmol.path_registry.ckpt_pdb}/{small_molecules[0]}.pdb")
     time_after = os.path.getmtime(
         f"{packmol.path_registry.ckpt_pdb}/{small_molecules[0]}.pdb"
     )
