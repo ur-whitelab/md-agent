caf733096c16b491f2a3849eed4a9a28b1d2ce8b
Jorge <jmedina9@ur.rochester.edu>
2024-05-27 11:26:33 -0400

    splitting unit tests for good and bad inputs, add test for cosine content util function

--- tests/test_pca_tool.py
+++ tests/test_pca_tool.py
@@ -1,18 +1,14 @@
 from unittest.mock import patch
 
+import numpy as np
+
 from mdagent.tools.base_tools import PCATool
+from mdagent.tools.base_tools.analysis_tools.pca_tools import PCA_analysis
 
 
-def test_pca_tool(get_registry):
+def test_pca_tool_bad_inputs(get_registry):
     reg = get_registry("raw", True, dynamic=True, include_hydrogens=True)
     pca_tool = PCATool(path_registry=reg)
-    good_inputs = {
-        "trajectory_fileid": "rec0_butane_123456",
-        "topology_fileid": "top_sim0_butane_123456",
-        "pc_percentage": "95",
-        "analysis": "all",
-        "selection": "all",
-    }
     bad_input_files = {
         "trajectory_fileid": "rec0_butane_456456",
         "topology_fileid": "top_sim0_butane_456456",
@@ -25,6 +21,19 @@ def test_pca_tool(get_registry):
     assert "Trajectory File ID not in path registry" in error_catching
     assert "Topology File ID not in path registry" in error_catching
     assert "pc_percentage value must be a float" in error_catching
+
+
+def test_pca_tool_good_inputs(get_registry):
+    reg = get_registry("raw", True, dynamic=True, include_hydrogens=True)
+    pca_tool = PCATool(path_registry=reg)
+    good_inputs = {
+        "trajectory_fileid": "rec0_butane_123456",
+        "topology_fileid": "top_sim0_butane_123456",
+        "pc_percentage": "95",
+        "analysis": "all",
+        "selection": "name CA",
+    }
+
     with patch("matplotlib.pyplot.figure"), patch("matplotlib.pyplot.plot"), patch(
         "matplotlib.pyplot.xlabel"
     ), patch("matplotlib.pyplot.ylabel"), patch("matplotlib.pyplot.title"), patch(
@@ -44,3 +53,20 @@ def test_pca_tool(get_registry):
     # This two tests involve matplotlib, and are annoying to get done.
     # assert "PCA plots saved as" in result
     # assert "Scree Plot saved as" in result
+
+
+def test_cosine_content(get_registry):
+    reg = get_registry("raw", True, dynamic=True, include_hydrogens=True)
+    pca_analysis = PCA_analysis(
+        path_registry=reg,
+        pc_percentage=95,
+        top_path=reg.get_path("top_sim0_butane_123456"),
+        traj_path=reg.get_path("rec0_butane_123456"),
+        sim_id="sim0_butane_123456",
+        selection="name CA",
+    )
+    pca_space = np.random.rand(100, 2)
+    T = len(pca_space)
+    np.arange(T)
+    assert pca_analysis._cosine_content(pca_space=pca_space, i=0) < 0.1
+    assert pca_analysis._cosine_content(pca_space=pca_space, i=1) < 0.1
