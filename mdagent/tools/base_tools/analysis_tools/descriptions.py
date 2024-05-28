990a0d7d519f0c0701131bc7fd4d953495d11e40
Jorge <jmedina9@ur.rochester.edu>
2024-05-28 17:58:59 -0400

    adding helper fucntions for the distance tool: assembling the distance matrix and or the distance histogram

--- mdagent/tools/base_tools/analysis_tools/descriptions.py
+++ mdagent/tools/base_tools/analysis_tools/descriptions.py
@@ -4,14 +4,37 @@ TOPOLOGY_FILEID_DESC = "Topology File ID of the simulation to be analyzed"
 
 SELECTION_DESC = "Selection of atoms from the simulation to use for the analysis"
 
+RES_SELECTION_DESC = (
+    "Selection of residues ids (resids) from the simulation to use for the analysis"
+)
+CONTACT_SELECTION_DESC = (
+    "Selection of residues from the simulation" " to use for the contact analysis"
+)
+
 CUTOFF_DESC = "Cutoff distance for the analysis in angstroms"
 
 #####################Tool Descriptions#####################
 DISTANCE_TOOL_DESC = (
-    "Tool for calculating distances between" " atom selections pairs in a trajectory."
+    "Tool for calculating distances between residue pairs in each frame of a"
+    " trajectory. If only one pair is provided, the tool will calculate the distance"
+    " between said pair in each frame and output a distance vs time plot and a "
+    " histogram. iF multiple pairs are provided, the tool will calculate the distance "
+    "between each pair in each frame and output a distance matrix plot for the selected"
+    " pairs.\n You can use mode analysis = 'all' to calculate the distance between all "
+    "residue pairs in each frame. Or if interested in a specific pair, you can provide "
+    "two selections of residues/atoms to calculate the distance between them."
 )
 
 DISPLACEMENT_TOOL_DESC = (
     "Tool for calculating displacement vectors"
     " between atom selections pairs in a trajectory."
+)
+NEIGHBORS_TOOL_DESC = (
+    "Find (spatially) neighboring atoms in a trajectory.\n\n"
+    "Given a selection of atoms and a distance cutoff, computes the indices of all"
+    " atoms whose distance to 1 or more of the query points is less than cutoff."
+)
+
+CONTACTS_TOOL_DESC = (
+    "Tool for computing the distance between pairs of residues in a trajectory."
 )
--- mdagent/tools/base_tools/analysis_tools/distance_tools.py
+++ mdagent/tools/base_tools/analysis_tools/distance_tools.py
@@ -1,14 +1,20 @@
+import itertools
 from typing import Optional
 
+import mdtraj as md
+import numpy as np
 from langchain.tools import BaseTool
 from pydantic import BaseModel, Field
 
 from mdagent.utils import PathRegistry
 
 from .descriptions import (
+    CONTACT_SELECTION_DESC,
+    CONTACTS_TOOL_DESC,
     CUTOFF_DESC,
     DISPLACEMENT_TOOL_DESC,
     DISTANCE_TOOL_DESC,
+    NEIGHBORS_TOOL_DESC,
     SELECTION_DESC,
     TOPOLOGY_FILEID_DESC,
     TRAJECTORY_FILEID_DESC,
@@ -16,12 +22,128 @@ from .descriptions import (
 
 
 class distanceUtils:
-    pass
+    def all_possible_pairs(self, array1, array2):
+        return list(itertools.product(array1, array2))
+
+    def calc_residue_dist(self, residues=(0, 0)):
+        """
+        Return the C-alpha distance between two residues.
+        """
+        diff_vector = residues[0] - residues[1]
+        return np.sqrt(np.vdot(diff_vector, diff_vector))
+
+    def calc_side_center_mass(self, traj, topology, frame):
+        """
+        Return approximate center of mass of each side chain.
+        COM od Glycine is approximated by the coordinate of its CA atom.
+        """
+        scmass = []
+
+        for i in range(0, topology.n_residues):
+            selection = topology.select("(resid %d) and sidechain" % i)
+            if len(selection) < 1:
+                selection = topology.select("(resid %d) and (name CA)" % i)
+            mass = np.array([0.0, 0.0, 0.0])
+            for atom in selection:
+                mass += traj.xyz[frame, atom, :]
+            scmass.append(mass / len(selection))
+        return scmass
+
+    def calc_residue_side_dist(self, traj, frame, residue_one, residue_two):
+        """
+        Return the C-alpha distance between two residues.
+        """
+        # Select first residue
+        selection1 = traj.topology.select("(resid %d) and sidechain" % residue_one)
+        if len(selection1) < 1:
+            selection1 = traj.topology.select("(resid %d) and (name CA)" % residue_one)
+        # Select second residue
+        selection2 = traj.topology.select("(resid %d) and sidechain" % residue_two)
+        if len(selection2) < 1:
+            selection2 = traj.topology.select("(resid %d) and (name CA)" % residue_two)
+        atom_pairs = self.all_possible_pairs(
+            traj.xyz[frame, selection1, :], traj.xyz[frame, selection2, :]
+        )
+        return min(map(self.calc_residue_dist, atom_pairs))
+
+    def calc_matrix_cm(self, traj, frame, threshold=0.8, distance=1.2):
+        """
+        Used internally to compute the matrix data when the object is
+        initialized.
+        """
+        selection = traj.topology.select("name CA")
+        scmass = self.calc_side_center_mass(traj.topology, frame)
+        dim = len(selection)
+
+        matrix = np.zeros(dim, dim)
+        for row, atom1 in enumerate(selection):
+            for col, atom2 in enumerate(selection):
+                # Only calculate once
+                if col > row - 1:
+                    continue
+
+                # the atom pointprint(traj.xyz[0, atom, :])
+                val = self.calc_residue_dist(
+                    residues=(traj.xyz[frame, atom1, :], traj.xyz[frame, atom2, :])
+                )
+                # Center of mass
+
+                dis = self.calc_residue_dist((scmass[col], scmass[row]))
+
+                matrix[row, col] = val < threshold
+
+                if dis < distance:
+                    matrix[col, row] = 1 - dis / distance
+                else:
+                    matrix[col, row] = 0
+
+        return matrix
+
+    def calc_matrix_dis(self, traj, frame, threshold=0.8, distance=(0.5, 1.2)):
+        """
+        Used internally to compute the matrix data when the object is
+        initialized.
+        """
+        selection = traj.topology.select("name CA")
+        self.calc_side_center_mass(traj.topology, frame)
+        len(selection)
+
+        matrix = np.zeros((len(selection), len(selection)))  # , np.bool)
+        for row, atom1 in enumerate(selection):
+            for col, atom2 in enumerate(selection):
+                # Only calculate once
+                if col > row - 1:
+                    continue
+
+                # the atom pointprint(traj.xyz[0, atom, :])
+                val = self.calc_residue_dist(
+                    residues=(traj.xyz[frame, atom1, :], traj.xyz[frame, atom2, :])
+                )
+                # closest distance of sidechains
+                dis = self.calc_residue_side_dist(frame, row, col)
+
+                matrix[row, col] = val < threshold
+
+                if dis < distance[0]:
+                    matrix[col, row] = 1
+                elif distance[0] < dis < distance[1]:
+                    matrix[col, row] = (distance[1] - dis) / (distance[1] - distance[0])
+        return matrix
 
 
 class distanceSchema(BaseModel):
     trajectory_fileid: str = Field(description=TRAJECTORY_FILEID_DESC)
     topology_fileid: str = Field(description=TOPOLOGY_FILEID_DESC)
+    analysis: str = Field(
+        "all", description="Which residues to calculate distance from"
+    )
+    mode: str = Field(
+        "CA",
+        description=(
+            "What to use for distance calculation, either "
+            "alpha carbons (CA) or center of mass (COM)"
+        ),
+    )
     selection1: str = Field(description="First" + SELECTION_DESC)
     selection2: str = Field(description="Second" + SELECTION_DESC)
 
@@ -36,16 +158,14 @@ class displacementSchema(BaseModel):
 class neighborsSchema(BaseModel):
     trajectory_fileid: str = Field(description=TRAJECTORY_FILEID_DESC)
     topology_fileid: str = Field(description=TOPOLOGY_FILEID_DESC)
-    selection1: str = Field(description="First" + SELECTION_DESC)
-    selection2: str = Field(description="Second" + SELECTION_DESC)
+    selection: str = Field(description=SELECTION_DESC)
     cutoff: float = Field(10.0, description=CUTOFF_DESC)
 
 
 class contactSchema(BaseModel):
     trajectory_fileid: str = Field(description=TRAJECTORY_FILEID_DESC)
     topology_fileid: str = Field(description=TOPOLOGY_FILEID_DESC)
-    selection1: str = Field(description="First" + SELECTION_DESC)
-    selection2: str = Field(description="Second" + SELECTION_DESC)
+    selection: str = Field(description=CONTACT_SELECTION_DESC)
     cutoff: float = Field(6.0, description=CUTOFF_DESC)
 
 
@@ -60,8 +180,29 @@ class distanceTool(BaseTool):
         self.path_registry = path_registry
 
     def _run(self, **input):
-        input = self.validate_input(input)
-        pass
+        try:
+            input = self.validate_input(**input)
+        except ValueError as e:
+            return f"Error using the PCA Tool: {str(e)}"
+
+        error = input.get("error", None)
+        if error:
+            return f"Error with the tool inputs: {error} "
+        input.get("system_message")
+
+        trajectory_id = input["trajectory_fileid"]
+        topology_id = input["topology_fileid"]
+        selection1 = input["selection1"]
+        selection2 = input["selection2"]
+
+        path_to_traj = self.path_registry.get_mapped_path(trajectory_id)
+        path_to_top = self.path_registry.get_mapped_path(topology_id)
+        traj = md.load(path_to_traj, top=path_to_top)
+        atom_indices1 = traj.top.select(selection1)
+        atom_indices2 = traj.top.select(selection2)
+        utils = distanceUtils()
+        pairs = utils.all_possible_pairs(atom_indices1, atom_indices2)
+        md.compute_distances(traj, pairs)
 
     def _arun(self):
         pass
@@ -71,16 +212,43 @@ class distanceTool(BaseTool):
         input = input.get("input", input)
         trajectory_id = input.get("trajectory_fileid", None)
         topology_id = input.get("topology_fileid", None)
+        selection1 = input.get("selection1", "name CA")
+        selection2 = input.get("selection2", "name CA")
         if not trajectory_id:
             raise ValueError("Incorrect Inputs: trajectory_fileid is required")
         if not topology_id:
             raise ValueError("Incorrect Inputs: topology_fileid is required")
         fileids = self.path_registry.list_path_names()
         error = ""
+        system_message = ""
         if trajectory_id not in fileids:
             error += " Trajectory File ID not in path registry"
         if topology_id not in fileids:
             error += " Topology File ID not in path registry"
+
+        keys = input.keys()
+        for key in keys:
+            if key not in [
+                "trajectory_fileid",
+                "topology_fileid",
+                "analysis",
+                "mode",
+                "selection1",
+                "selection2",
+                "remove_terminals",
+            ]:
+                system_message += f"{key} is not part of admitted tool inputs"
+
+        if error == "":
+            error = None
+        return {
+            "trajectory_fileid": trajectory_id,
+            "topology_fileid": topology_id,
+            "selection1": selection1,
+            "selection2": selection2,
+            "error": error,
+            "system_message": system_message,
+        }
 
 
 class displacementTool(BaseTool):
@@ -104,24 +272,45 @@ class displacementTool(BaseTool):
         input = input.get("input", input)
         trajectory_id = input.get("trajectory_fileid", None)
         topology_id = input.get("topology_fileid", None)
+        selection1 = input.get("selection1", "name CA")
+        selection2 = input.get("selection2", "name CA")
         if not trajectory_id:
             raise ValueError("Incorrect Inputs: trajectory_fileid is required")
         if not topology_id:
             raise ValueError("Incorrect Inputs: topology_fileid is required")
         fileids = self.path_registry.list_path_names()
         error = ""
+        system_message = ""
         if trajectory_id not in fileids:
             error += " Trajectory File ID not in path registry"
         if topology_id not in fileids:
             error += " Topology File ID not in path registry"
+        keys = input.keys()
+        for key in keys:
+            if key not in [
+                "trajectory_fileid",
+                "topology_fileid",
+                "pc_percentage",
+                "analysis",
+                "selection",
+                "remove_terminals",
+            ]:
+                system_message += f"{key} is not part of admitted tool inputs"
+        if error == "":
+            error = None
+        return {
+            "trajectory_fileid": trajectory_id,
+            "topology_fileid": topology_id,
+            "selection1": selection1,
+            "selection2": selection2,
+            "error": error,
+            "system_message": system_message,
+        }
 
 
 class neighborsTool(BaseTool):
     name = "neighborsTool"
-    description = (
-        "Tool for calculating number of neighbors"
-        " on an atom selection in a trajectory."
-    )
+    description = NEIGHBORS_TOOL_DESC
     input_schema = neighborsSchema
     path_registry: Optional[PathRegistry]
 
@@ -140,43 +329,94 @@ class neighborsTool(BaseTool):
         input = input.get("input", input)
         trajectory_id = input.get("trajectory_fileid", None)
         topology_id = input.get("topology_fileid", None)
-        if not trajectory_id:
-            raise ValueError("Incorrect Inputs: trajectory_fileid is required")
-        if not topology_id:
-            raise ValueError("Incorrect Inputs: topology_fileid is required")
-
-
-class contactsTool(BaseTool):
-    name = "NeighborsTool"
-    description = (
-        "Tool for calculating number of contacts"
-        " between pairs of residues in a trajectory."
-    )
-    input_schema = neighborsSchema
-    path_registry: Optional[PathRegistry]
-
-    def __init__(self, path_registry: Optional[PathRegistry] = None):
-        super().__init__()
-        self.path_registry = path_registry
-
-    def _run(self):
-        pass
-
-    def _arun(self):
-        pass
-
-    def validate_inputs(self, input):
-        input = input.get("action_input", input)
-        input = input.get("input", input)
-        trajectory_id = input.get("trajectory_fileid", None)
-        topology_id = input.get("topology_fileid", None)
+        selection = input.get("selection", "name CA")
+        cutoff = input.get("cutoff", 10.0)
         if not trajectory_id:
             raise ValueError("Incorrect Inputs: trajectory_fileid is required")
         if not topology_id:
             raise ValueError("Incorrect Inputs: topology_fileid is required")
         fileids = self.path_registry.list_path_names()
         error = ""
+        system_message = ""
         if trajectory_id not in fileids:
             error += " Trajectory File ID not in path registry"
         if topology_id not in fileids:
             error += " Topology File ID not in path registry"
+        keys = input.keys()
+        for key in keys:
+            if key not in [
+                "trajectory_fileid",
+                "topology_fileid",
+                "pc_percentage",
+                "analysis",
+                "selection",
+                "remove_terminals",
+            ]:
+                system_message += f"{key} is not part of admitted tool inputs"
+        if error == "":
+            error = None
+        return {
+            "trajectory_fileid": trajectory_id,
+            "topology_fileid": topology_id,
+            "selection": selection,
+            "cutoff": cutoff,
+            "error": error,
+            "system_message": system_message,
+        }
+
+
+class contactsTool(BaseTool):
+    name = "NeighborsTool"
+    description = CONTACTS_TOOL_DESC
+    input_schema = neighborsSchema
+    path_registry: Optional[PathRegistry]
+
+    def __init__(self, path_registry: Optional[PathRegistry] = None):
+        super().__init__()
+        self.path_registry = path_registry
+
+    def _run(self):
+        pass
+
+    def _arun(self):
+        pass
+
+    def validate_inputs(self, input):
+        input = input.get("action_input", input)
+        input = input.get("input", input)
+        trajectory_id = input.get("trajectory_fileid", None)
+        topology_id = input.get("topology_fileid", None)
+        selection = input.get("selection", "all")
+        cutoff = input.get("cutoff", 6.0)
+        if not trajectory_id:
+            raise ValueError("Incorrect Inputs: trajectory_fileid is required")
+        if not topology_id:
+            raise ValueError("Incorrect Inputs: topology_fileid is required")
+        fileids = self.path_registry.list_path_names()
+        error = ""
+        system_message = ""
+        if trajectory_id not in fileids:
+            error += " Trajectory File ID not in path registry"
+        if topology_id not in fileids:
+            error += " Topology File ID not in path registry"
+        keys = input.keys()
+        for key in keys:
+            if key not in [
+                "trajectory_fileid",
+                "topology_fileid",
+                "pc_percentage",
+                "analysis",
+                "selection",
+                "remove_terminals",
+            ]:
+                system_message += f"{key} is not part of admitted tool inputs"
+        if error == "":
+            error = None
+        return {
+            "trajectory_fileid": trajectory_id,
+            "topology_fileid": topology_id,
+            "selection": selection,
+            "cutoff": cutoff,
+            "error": error,
+            "system_message": system_message,
+        }
