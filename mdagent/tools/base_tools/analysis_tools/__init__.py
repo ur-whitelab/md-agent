4072ed7eef9889ee93c0c84fbc3d395344069dd3
qcampbel <qcampbe2@ur.rochester.edu>
2024-06-06 18:10:02 -0400

    adjusted the rest of init & test files

--- mdagent/tools/base_tools/analysis_tools/__init__.py
+++ mdagent/tools/base_tools/analysis_tools/__init__.py
@@ -5,12 +5,12 @@ from .rmsd_tools import RMSDCalculator
 from .vis_tools import VisFunctions, VisualizeProtein
 
 __all__ = [
-    "SimulationOutputFigures",
     "PPIDistance",
     "RadiusofGyrationAverage",
     "RadiusofGyrationPerFrame",
     "RadiusofGyrationPlot",
     "RMSDCalculator",
+    "SimulationOutputFigures",
     "VisFunctions",
     "VisualizeProtein",
 ]
