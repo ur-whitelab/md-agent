d9285c12c8ae38ee55fe0130dbbf0826c7d59e6a
Sam Cox <swrig30@ur.rochester.edu>
2024-06-24 20:37:10 -0700

    more docstrings

--- mdagent/tools/base_tools/analysis_tools/secondary_structure.py
+++ mdagent/tools/base_tools/analysis_tools/secondary_structure.py
@@ -6,7 +6,21 @@ from langchain.tools import BaseTool
 from mdagent.utils import FileType, PathRegistry, load_single_traj
 
 
-def write_raw_x(x, values, traj_id, path_registry):
+def write_raw_x(
+    x: str, values: np.ndarray, traj_id: str, path_registry: PathRegistry
+) -> str:
+    """
+    Writes raw x values to a file and saves the file to the path registry.
+
+    Args:
+        x: The name of the analysis tool that produced the values (e.g., "dssp")
+        values: The x values to save.
+        traj_id: The id of the trajectory the values are associated with.
+        path_registry: The path registry to save the file to.
+
+        Returns:
+            The file id of the saved file.
+    """
     file_name = path_registry.write_file_name(
         FileType.RECORD,
         record_type=x,
@@ -30,20 +44,28 @@ class ComputeDSSP(BaseTool):
     for a protein trajectory. Input is a trajectory file (e.g., .xtc, .
     trr) and an optional topology file (e.g., .pdb, .prmtop). The output
     is an array with the DSSP code for each residue at each time point."""
-    path_registry: PathRegistry | None = None
+    path_registry: PathRegistry = PathRegistry.get_instance()
     simplified: bool = True
 
-    def __init__(self, path_registry, simplified: bool = True):
+    def __init__(self, path_registry: PathRegistry, simplified: bool = True):
         super().__init__()
         self.path_registry = path_registry
         self.simplified = simplified
 
-    def _dssp_codes(self):
+    def _dssp_codes(self) -> list[str]:
+        """
+        Returns the DSSP codes used by MDTraj. If simplified is True, only
+        the codes H, E, and C are used. Otherwise, the full set of codes is
+        used."""
         if self.simplified:
             return ["H", "E", "C"]
         return ["H", "B", "E", "G", "I", "T", "S", " "]
 
-    def _dssp_natural_language(self):
+    def _dssp_natural_language(self) -> dict[str, str]:
+        """
+        Returns a dictionary mapping DSSP codes to their natural language
+        descriptions. If simplified is True, only the codes H, E, and C are
+        used. Otherwise, the full set of codes is used."""
         if self.simplified:
             return {"H": "helix", "E": "strand", "C": "coil"}
         return {
@@ -57,7 +79,18 @@ class ComputeDSSP(BaseTool):
             " ": "loop or irregular",
         }
 
-    def _convert_dssp_counts(self, dssp_counts: dict):
+    def _convert_dssp_counts(self, dssp_counts: dict) -> dict:
+        """
+        Converts a dictionary of DSSP codes to their counts into a dictionary
+        of natural language descriptions to their counts.
+        is used.
+
+        Args:
+            dssp_counts: A dictionary mapping DSSP codes to their counts.
+
+        Returns:
+            A dictionary mapping natural language descriptions to their counts.
+        """
         code_to_description = self._dssp_natural_language()
 
         descriptive_counts = {
@@ -65,19 +98,48 @@ class ComputeDSSP(BaseTool):
         }
         return descriptive_counts
 
-    def _summarize_dssp(self, dssp_array):
+    def _summarize_dssp(self, dssp_array: np.ndarray) -> dict[str, int]:
+        """
+        Summarizes the DSSP assignments for a trajectory. Returns a dictionary
+        mapping DSSP codes to their counts.
+
+        Args:
+            dssp_array: An array of DSSP codes for each residue at each time point.
+
+        Returns:
+            A dictionary mapping natural language descriptions to their counts.
+        """
         dssp_codes = self._dssp_codes()
-        # turn into dict where keys are codes
         dssp_dict = {code: 0 for code in dssp_codes}
         for frame in dssp_array:
             for code in frame:
                 dssp_dict[code] += 1
         return self._convert_dssp_counts(dssp_dict)
 
-    def _compute_dssp(self, traj):
+    def _compute_dssp(self, traj: md.Trajectory) -> np.ndarray:
+        """
+        Computes the DSSP assignments for a trajectory.
+
+        Args:
+            traj: The trajectory to compute DSSP assignments for.
+
+        Returns:
+            An array of DSSP codes for each residue at each time point.
+        """
         return md.compute_dssp(traj, simplified=self.simplified)
 
-    def _run(self, traj_file, top_file=None):
+    def _run(self, traj_file: str, top_file: str | None = None) -> str:
+        """
+        Computes the DSSP assignments for a trajectory and saves the results
+        to a file.
+
+        Args:
+            traj_file: The file id of the trajectory to compute DSSP assignments for.
+            top_file: The file id of the topology file to use.
+
+        Returns:
+            A summary of the DSSP assignments.
+        """
         try:
             traj = load_single_traj(
                 path_registry=self.path_registry,
@@ -92,9 +154,10 @@ class ComputeDSSP(BaseTool):
         dssp_array = self._compute_dssp(traj)
         write_raw_x("dssp", dssp_array, traj_file, self.path_registry)
         summary = self._summarize_dssp(dssp_array)
-        return summary
+        return str(summary)
 
-    async def _arun(self, traj_file, top_file=None):
+    async def _arun(self, traj_file, top_file):
+        """Runs the tool asynchronously."""
         raise NotImplementedError("Async version not implemented")
 
 
@@ -105,16 +168,36 @@ class ComputeGyrationTensor(BaseTool):
     xtc, .trr) and an optional topology file (e.g., .pdb, .prmtop).
     The output is an array of gyration tensors for each frame of the
     trajectory."""
-    path_registry: PathRegistry | None = None
+    path_registry: PathRegistry = PathRegistry.get_instance()
 
     def __init__(self, path_registry: PathRegistry):
         super().__init__()
         self.path_registry = path_registry
 
-    def _compute_gyration_tensor(self, traj):
+    def _compute_gyration_tensor(self, traj: md.Trajectory) -> np.ndarray:
+        """
+        Computes the gyration tensor for a trajectory.
+
+        Args:
+            traj: The trajectory to compute the gyration tensor for.
+
+        Returns:
+            An array of gyration tensors for each frame of the trajectory.
+        """
         return md.compute_gyration_tensor(traj)
 
-    def _run(self, traj_file, top_file=None):
+    def _run(self, traj_file: str, top_file: str | None = None) -> str:
+        """
+        Computes the gyration tensor for a trajectory and saves the results
+        to a file.
+
+        Args:
+            traj_file: The file id of the trajectory to compute the gyration tensor for.
+            top_file: The file id of the topology file to use.
+
+        Returns:
+            A message indicating the success of the computation
+        """
         try:
             traj = load_single_traj(
                 path_registry=self.path_registry,
@@ -133,10 +216,25 @@ class ComputeGyrationTensor(BaseTool):
         return "Gyration tensor computed successfully, " f"saved to {file_id}"
 
     async def _arun(self, traj_file, top_file=None):
+        """Runs the tool asynchronously."""
         raise NotImplementedError("Async version not implemented")
 
 
-def plot_x_over_time(x, values, traj_id, path_registry):
+def plot_x_over_time(
+    x: str, values: np.ndarray, traj_id: str, path_registry: PathRegistry
+) -> str:
+    """
+    Plots the values of x over time and saves the plot to a file.
+
+    Args:
+        x: The name of the analysis tool that produced the values (e.g., "dssp")
+        values: The x values to plot.
+        traj_id: The id of the trajectory the values are associated with.
+        path_registry: The path registry to save the file to.
+
+    Returns:
+        The file id of the saved file.
+    """
     plt.figure(figsize=(10, 6))
     plt.plot(values)
     plt.xlabel("Frame")
@@ -169,16 +267,35 @@ class ComputeAsphericity(BaseTool):
     xtc, .trr) and an optional topology file (e.g., .pdb, .prmtop).
     The output is asphericity values for each frame of the
     trajectory."""
-    path_registry: PathRegistry | None = None
+    path_registry: PathRegistry = PathRegistry.get_instance()
 
     def __init__(self, path_registry: PathRegistry):
         super().__init__()
         self.path_registry = path_registry
 
-    def _compute_asphericity(self, traj):
+    def _compute_asphericity(self, traj: md.Trajectory) -> np.ndarray:
+        """
+        Computes the asphericity for a trajectory.
+
+        Args:
+            traj: The trajectory to compute the asphericity for.
+
+        Returns:
+            An array of asphericity values for each frame of the trajectory."""
         return md.asphericity(traj)
 
-    def _run(self, traj_file, top_file=None):
+    def _run(self, traj_file: str, top_file: str | None = None) -> str:
+        """
+        Computes the asphericity for a trajectory and saves the results
+        to a file.
+
+        Args:
+            traj_file: The file id of the trajectory to compute the asphericity for.
+            top_file: The file id of the topology file to use.
+
+        Returns:
+            A message indicating the success of the computation.
+        """
         try:
             traj = load_single_traj(
                 path_registry=self.path_registry,
@@ -202,7 +319,8 @@ class ComputeAsphericity(BaseTool):
             f"{plot_file_id}"
         )
 
-    async def _arun(self, traj_file, top_file=None):
+    async def _arun(self, traj_file, top_file):
+        """Runs the tool asynchronously."""
         raise NotImplementedError("Async version not implemented")
 
 
@@ -213,16 +331,36 @@ class ComputeAcylindricity(BaseTool):
     xtc, .trr) and an optional topology file (e.g., .pdb, .prmtop). The
     output is an array of acylindricity values for each frame of the
     trajectory."""
-    path_registry: PathRegistry | None = None
+    path_registry: PathRegistry = PathRegistry.get_instance()
 
     def __init__(self, path_registry: PathRegistry):
         super().__init__()
         self.path_registry = path_registry
 
-    def _compute_acylindricity(self, traj):
+    def _compute_acylindricity(self, traj: md.Trajectory) -> np.ndarray:
+        """
+        Computes the acylindricity for a trajectory.
+
+        Args:
+            traj: The trajectory to compute the acylindricity for.
+
+        Returns:
+            An array of acylindricity values for each frame of the trajectory.
+        """
         return md.acylindricity(traj)
 
-    def _run(self, traj_file, top_file=None):
+    def _run(self, traj_file: str, top_file: str | None = None) -> str:
+        """
+        Computes the acylindricity for a trajectory and saves the results
+        to a file.
+
+        Args:
+            traj_file: The file id of the trajectory to compute the acylindricity for.
+            top_file: The file id of the topology file to use.
+
+        Returns:
+            A message indicating the success of the computation.
+        """
         try:
             traj = load_single_traj(
                 path_registry=self.path_registry,
@@ -238,7 +376,7 @@ class ComputeAcylindricity(BaseTool):
             "acylindricity", acylindricity_values, traj_file, self.path_registry
         )
         plot_file_id = plot_x_over_time(
-            "acylindricity", acylindricity_values, traj_file
+            "acylindricity", acylindricity_values, traj_file, self.path_registry
         )
         return (
             "acylindricity_values saved to "
@@ -246,7 +384,8 @@ class ComputeAcylindricity(BaseTool):
             f"{plot_file_id}"
         )
 
-    async def _arun(self, traj_file, top_file=None):
+    async def _arun(self, traj_file, top_file):
+        """Runs the tool asynchronously."""
         raise NotImplementedError("Async version not implemented")
 
 
@@ -257,17 +396,38 @@ class ComputeRelativeShapeAntisotropy(BaseTool):
     file (e.g., .xtc, .trr) and an optional topology file (e.g., .pdb, .
     prmtop). The output is an array of relative shape antisotropy values
     for each frame of the trajectory."""
-    # TODO -> should this write to a file or return the array?
-    path_registry: PathRegistry | None = None
+    path_registry: PathRegistry = PathRegistry.get_instance()
 
     def __init__(self, path_registry: PathRegistry):
         super().__init__()
         self.path_registry = path_registry
 
-    def _compute_relative_shape_antisotropy(self, traj):
+    def _compute_relative_shape_antisotropy(self, traj: md.Trajectory) -> np.ndarray:
+        """
+        Computes the relative shape antisotropy for a trajectory.
+
+        Args:
+            traj: The trajectory to compute the relative shape antisotropy for.
+
+        Returns:
+            An array of relative shape antisotropy
+                values for each frame of the trajectory.
+        """
         return md.relative_shape_antisotropy(traj)
 
-    def _run(self, traj_file, top_file=None):
+    def _run(self, traj_file: str, top_file: str | None = None) -> str:
+        """
+        Computes the relative shape antisotropy for a trajectory and saves the results
+        to a file.
+
+        Args:
+            traj_file: The file id of the trajectory to
+                compute the relative shape antisotropy for.
+            top_file: The file id of the topology file to use.
+
+        Returns:
+            A message indicating the success of the computation.
+        """
         try:
             traj = load_single_traj(
                 path_registry=self.path_registry,
@@ -289,7 +449,10 @@ class ComputeRelativeShapeAntisotropy(BaseTool):
             self.path_registry,
         )
         plot_file_id = plot_x_over_time(
-            "relative_shape_antisotropy", relative_shape_antisotropy_values, traj_file
+            "relative_shape_antisotropy",
+            relative_shape_antisotropy_values,
+            traj_file,
+            self.path_registry,
         )
         return (
             "relative_shape_antisotropy_values saved to "
@@ -297,5 +460,6 @@ class ComputeRelativeShapeAntisotropy(BaseTool):
             f"{plot_file_id}"
         )
 
-    async def _arun(self, traj_file, top_file=None):
+    async def _arun(self, traj_file, top_file):
+        """Runs the tool asynchronously."""
         raise NotImplementedError("Async version not implemented")
