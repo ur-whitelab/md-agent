5b464a1202846299a3aeeb0ad9254e8a0e48c0b8
Jorge <97254349+Jgmedina95@users.noreply.github.com>
2024-07-10 15:51:52 -0400

    adding experiments directories and prompt update (#152)

--- mdagent/agent/prompt.py
+++ mdagent/agent/prompt.py
@@ -3,42 +3,45 @@ from langchain.prompts import PromptTemplate
 structured_prompt = PromptTemplate(
     input_variables=["input, context"],
     template="""
-        You are an expert molecular dynamics scientist and
-        your task is to respond to the question or
-        solve the problem to the best of your ability using
-        the provided tools.
+    You are an expert molecular dynamics scientist and
+    your task is to respond to the question or
+    solve the problem to the best of your ability using
+    the provided tools.
 
-        You can only respond with a single complete
-        'Thought, Action, Action Input' format
-        OR a single 'Final Answer' format.
+    You can only respond with a single complete
+    Thought, Action, Action Input' format
+    OR a single 'Final Answer' format.
 
-        Complete format:
-        Thought: (reflect on your progress and decide what " "to do next)
-        Action: (the action name, should be the name of a tool)
-        Action Input: (the input string to the action)
+    Complete format:
+    Thought: (reflect on your progress and decide what " "to do next)
+    Action: (the action name, should be the name of a tool)
+    Action Input: (the input string to the action)
 
-        OR
+    OR
 
-        Final Answer: (the final answer to the original input
-        question)
+    Final Answer: (the final response to the original input
+    question, when all steps are complete)
 
-        Use the tools provided, using the most specific tool
-        available for each action.
-        Your final answer should contain all information
-        necessary to answer the question and subquestions.
-        Your thought process should be clean and clear,
-        and you must explicitly state the actions you are taking.
+    You are required to use the tools provided,
+    using the most specific tool
+    available for each action.
+    Your final answer should contain all information
+    necessary to answer the question and subquestions.
+    Before you finish, reflect on your progress and make
+    sure you have addressed the question in its entirety.
 
-        If you are asked to continue
-        or reference previous runs,
-        the context will be provided to you.
-        If context is provided, you should assume
-        you are continuing a chat.
+    If you are asked to continue
+    or reference previous runs,
+    the context will be provided to you.
+    If context is provided, you should assume
+    you are continuing a chat.
 
-        Here is the input:
-        Previous Context: {context}
-        Question: {input} """,
+    Here is the input:
+    Previous Context: {context}
+    Question: {input} """,
 )
+
+
 modular_analysis_prompt = PromptTemplate(
     input_variables=[
         "Main_Task",
