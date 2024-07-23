a0ba05da147a8e065ffc4a93b6c074837dbcada6
qcampbel <qcampbe2@ur.rochester.edu>
2024-07-22 23:54:16 -0400

    added param-specific prompts

--- mdagent/tools/base_tools/util_tools/search_tools.py
+++ mdagent/tools/base_tools/util_tools/search_tools.py
@@ -1,3 +1,4 @@
+import logging
 import os
 import re
 from typing import Optional
@@ -13,6 +14,16 @@ from pypdf.errors import PdfReadError
 from mdagent.utils import PathRegistry
 
 
+def configure_logging(path):
+    # to log all runtime errors from paperscraper, which can be noisy
+    log_file = os.path.join(path, "scraping_errors.log")
+    logging.basicConfig(
+        filename=log_file,
+        level=logging.ERROR,
+        format="%(asctime)s:%(levelname)s:%(message)s",
+    )
+
+
 def paper_scraper(search: str, pdir: str = "query") -> dict:
     try:
         return paperscraper.search_papers(search, pdir=pdir)
@@ -20,33 +31,56 @@ def paper_scraper(search: str, pdir: str = "query") -> dict:
         return {}
 
 
-def paper_search(llm, query, path_registry):
+def paper_search(llm, query, path_registry, rounds=1):
     prompt = langchain.prompts.PromptTemplate(
-        input_variables=["question"],
+        input_variables=["question", "rounds"],
         template="""
-        I would like to find scholarly papers to answer
-        this question: {question}. Your response must be at
-        most 10 words long.
-        'A search query that would bring up papers that can answer
-        this question would be: '""",
+        I would like to find papers to answer
+        this question: {question}.
+        Your response must have exact {rounds} of search query(ies),
+        separated by comma, and each query must be at most 5 words long,
+        nothing else. Fewer keywords is better.
+        If a long protein name is given, use different known name or break
+        down a long name for each query. For example,
+        For `procollagen C-endopeptidase enchancer 1', your response would
+        be: type 1 procollagen, procollagen C-proteinase,
+        procollagen C-terminal proteinase.
+
+        A list of search queries that would bring up papers that can answer
+        this question would be: """,
     )
 
     path = f"{path_registry.ckpt_files}/query"
     query_chain = langchain.chains.llm.LLMChain(llm=llm, prompt=prompt)
     if not os.path.isdir(path):
         os.mkdir(path)
-    search = query_chain.run(query)
-    print("\nSearch:", search)
-    papers = paper_scraper(search, pdir=f"{path}/{re.sub(' ', '', search)}")
+    configure_logging(path)
+    search = query_chain.run({"question": query, "rounds": rounds})
+    search_list = [item.strip() for item in search.split(",")]
+    papers = {}
+    for s in search_list:
+        print("\nSearch:", s)
+        print("scraping...")
+        try:
+            papers.update(paper_scraper(s, pdir=f"{path}/{re.sub(' ', '', search)}"))
+        except RuntimeError:
+            print("Service limit of retries reached. Moved on.")
+        print(len(papers), "papers scrapped so far.")
     return papers
 
 
-def scholar2result_llm(llm, query, path_registry, k=5, max_sources=2):
+def scholar2result_llm(llm, query, path_registry, k=5, max_sources=2, search_rounds=3):
     """Useful to answer questions that require
     technical knowledge. Ask a specific question."""
-    papers = paper_search(llm, query, path_registry)
+    papers = paper_search(llm, query, path_registry, rounds=search_rounds)
     if len(papers) == 0:
-        return "Failed. Not enough papers found"
+        print("No papers scraped. ")
+        return (
+            "Failed. Not enough papers found. "
+            "Refine your query by using fewer or different keywords to broaden. "
+            "Don't keep trying the exact same keywords over and over. "
+        )
+    print("Done scraping! Now processing the docs...")
     docs = paperqa.Docs(llm=llm.model_name)
     not_loaded = 0
     for path, data in papers.items():
@@ -55,15 +89,18 @@ def scholar2result_llm(llm, query, path_registry, k=5, max_sources=2):
         except (ValueError, FileNotFoundError, PdfReadError):
             not_loaded += 1
         except Exception as e:
+            # just to catch whatever else fail
             print(f"Cannot load this paper. {type(e).__name__}: {e}")
+            not_loaded += 1
 
     print(
-        f"\nFound {len(papers)} papers"
+        f"\nScraped {len(papers)} papers"
         + (f" but couldn't load {not_loaded}" if not_loaded > 0 else "")
     )
+    print("Now answering this question...")
     answer = docs.query(query, k=k, max_sources=max_sources).formatted_answer
     print(answer)
-    return "Succeeded. " + answer
+    return answer
 
 
 class Scholar2ResultLLM(BaseTool):
